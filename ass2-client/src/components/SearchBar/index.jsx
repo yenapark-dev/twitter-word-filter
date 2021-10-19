9cde733ec584f5be557403bdc88b35c6f8ef49ac
Angel Wong <angel.wong@visionhq.io>
2021-10-19 18:18:15 +1000

    added search bar component

---
+++ ass2-client/src/components/SearchBar/index.jsx
@@ -0,0 +1,44 @@
+// Import libary
+import React, { useState } from 'react';
+
+// Import metieral ui components
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+
+// Import style sheets
+import './style.css';
+
+const SearchArea = (props) => {
+  const [input, setInput] = useState();
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setInput(value);
+  };
+
+  const submitKeyword = (values) => {
+    props.onAdd(input);
+    setInput('');
+    values.preventDefault();
+  };
+
+  return (
+    <div>
+      {/* form for search bar*/}
+      <form className='add-keyword'>
+        <input
+          name='keyword'
+          onChange={handleChange}
+          value={input}
+          placeholder='Enter your keyword here'
+        />
+
+        <Fab onClick={submitKeyword}>
+          <AddIcon />
+        </Fab>
+      </form>
+    </div>
+  );
+};
+
+export default SearchArea;
