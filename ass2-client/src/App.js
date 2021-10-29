ea5d9a0e9e62272543cbbd3140a21e2bbc22859c
Angel Wong <angel.wong@visionhq.io>
2021-10-30 03:10:44 +1000

    Added radio button to choose search method

--- ass2-client/src/App.js
+++ ass2-client/src/App.js
@@ -1,16 +1,22 @@
 // Import libray
-import React from 'react';
+import React, { useState } from 'react';
 
 // Import ant design style sheet
 import 'antd/dist/antd.css';
-
+import { Radio } from 'antd';
 // Import components
-import SearchBar from './components/SearchBar';
-
+import HashTagSearch from './components/HashTagSearch';
+import TextSearch from './components/TextSearch';
 // Import style sheet
 import './App.css';
 
 function App() {
+  const [searchBy, setSearchBy] = useState('hashtag');
+  const handdleOnChange = (event) => {
+    console.log(event.target.value);
+    setSearchBy(event.target.value);
+  };
+  console.log(searchBy);
   return (
     <div className='App'>
       <div className='Intro'>
@@ -24,7 +30,18 @@ function App() {
           by the user.
         </p>
       </div>
-      <SearchBar />
+      <div>
+        <Radio.Group onChange={handdleOnChange}>
+          <Radio.Button value='text' key='text'>
+            Text
+          </Radio.Button>
+          <Radio.Button value='hashtag' key='hashtag'>
+            Hashtag
+          </Radio.Button>
+        </Radio.Group>
+      </div>
+
+      {searchBy === 'text' ? <TextSearch /> : <HashTagSearch />}
     </div>
   );
 }
---
+++ ass2-client/src/components/HashTagSearch/index.jsx
@@ -0,0 +1,117 @@
+// Import libary
+import React, { useState } from 'react';
+
+// Import ant design  components
+import { Row, Col, Button, Input } from 'antd';
+import { SearchOutlined, PlusCircleOutlined } from '@ant-design/icons';
+
+// Import componets
+import Keywords from '../Keywords';
+
+// Import Services
+import { fetchTwitter } from '../../services/api';
+
+// Import style sheets
+import './style.css';
+
+const HashTagSearch = () => {
+  const [input, setInput] = useState({ value: '' });
+  const [keywords, setKeywords] = useState([]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInput((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  };
+
+  const submitKeyword = (event) => {
+    setKeywords((prevKeyword) => {
+      return [...prevKeyword, input];
+    });
+    setInput({ value: '' });
+    event.preventDefault();
+  };
+
+  const deleteQuery = (id) => {
+    setKeywords((prevQuery) => {
+      return prevQuery.filter((query, index) => {
+        return index !== id;
+      });
+    });
+  };
+  const handleSubmit = () => {
+    fetchTwitter(keywords);
+  };
+
+  return (
+    <div className='search-area'>
+      <Row>
+        {/* Add keyword input */}
+        <Col span={8}>
+          <Input
+            className='add-keyword'
+            name='value'
+            onChange={handleChange}
+            onPressEnter={submitKeyword}
+            suffix={
+              <PlusCircleOutlined
+                style={{ color: ' #f5ba13', fontSize: '2em' }}
+              />
+            }
+            allowClear
+            maxLength='512'
+            placeholder='Enter your query here'
+            value={input.value}
+            size='large'
+            style={{
+              height: '60px',
+              borderRadius: '7px',
+              boxShadow: '0 1px 5px rgb(138, 137, 137) ',
+            }}
+          />
+        </Col>
+        <Col span={8} />
+        {/* Search button */}
+        <Col span={8}>
+          <Button
+            block
+            icon={<SearchOutlined />}
+            onClick={handleSubmit}
+            size='large'
+            type='text'
+            style={{
+              height: '60px',
+              borderRadius: '7px',
+              boxShadow: '0 1px 5px rgb(138, 137, 137) ',
+              backgroundColor: ' #f5ba13',
+              fontSize: '1.2em',
+            }}
+          >
+            Search
+          </Button>
+        </Col>
+      </Row>
+      <Row className='keywords-area'>
+        {keywords
+          ? keywords.map((keyword, index) => {
+              return (
+                <Keywords
+                  key={index}
+                  id={index}
+                  name={keyword.value}
+                  content={keyword.value}
+                  onDelete={deleteQuery}
+                />
+              );
+            })
+          : null}
+      </Row>
+    </div>
+  );
+};
+
+export default HashTagSearch;
